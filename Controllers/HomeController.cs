dbad1bb39df03ccdbfd4d1c91f948440b90875a9
Garry Polley <garry.polley@c2fo.com>
2017-07-18 19:08:52 -0500

    fix the names of the files and project

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
-namespace DeveloperDashboard2.Controllers
+namespace DeveloperDashboard.Controllers
 {
     public class HomeController : Controller
     {
